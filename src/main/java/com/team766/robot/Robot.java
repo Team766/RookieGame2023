f9abc37d73ea86a46ba3020548aff35a584abbaf
Matthew1760 <146592032+Matthew1760@users.noreply.github.com>
2023-10-05 02:58:56 +0000

    Evan Smith fucked up the libraries

--- src/main/java/com/team766/robot/Robot.java
+++ src/main/java/com/team766/robot/Robot.java
@@ -3,10 +3,10 @@ package com.team766.robot;
 import com.team766.robot.mechanisms.*;
 
 public class Robot {
-	// Declare mechanisms here
+	public static Drive drive;
 
 
 	public static void robotInit() {
-		// Initialize mechanisms here
+		drive = new Drive();
 	}
 }
