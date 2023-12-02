335ad1c80001b1d57442fb3e38287dad0a399fc6
qntmcube <100043837+qntmcube@users.noreply.github.com>
2023-12-01 19:52:24 -0800

    fix

--- src/main/java/com/team766/robot/procedures/DriveStraight.java
+++ src/main/java/com/team766/robot/procedures/DriveStraight.java
@@ -7,8 +7,8 @@ import com.team766.robot.Robot;
 public class DriveStraight extends Procedure {
 	public void run(Context context) {
 		context.takeOwnership(Robot.drive);
-		Robot.drive.setDrivePower(0.25, 0.25);
-		context.waitForSeconds(1.0);
+		Robot.drive.setDrivePower(-0.25,-0.25);
+		context.waitForSeconds(3.0);
 		Robot.drive.setDrivePower(0.0, 0.0);
 	}
 }
