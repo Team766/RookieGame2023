4e0fc6f57c0ec5b45ab03ac9d2b9e842ca47eeb6
qntmcube <100043837+qntmcube@users.noreply.github.com>
2023-12-01 21:47:26 -0800

    fix

--- src/main/java/com/team766/robot/procedures/Kick.java
+++ src/main/java/com/team766/robot/procedures/Kick.java
@@ -7,6 +7,6 @@ public class Kick extends Procedure{
 	public void run(Context context) {
 		context.takeOwnership(Robot.kick);
 		Robot.kick.setKickPower(1.0);
-		context.waitForSeconds(1.0);
+		//context.waitForSeconds(1.0);
 	}
 }
